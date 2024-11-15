a64a211aa35b8874afddbafe0b04267c20d16d22
gabcytn <cayetanogabriel03@gmail.com>
2024-11-16 01:09:38 +0800

    Added mappings for /logout route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,6 +8,8 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get("/login", "AuthController::login");
 $routes->get("/register", "AuthController::register");
+$routes->get("/logout", "AuthController::logoutUser");
 
 $routes->post("/register", "AuthController::createUser");
 $routes->post("/login", "AuthController::checkUser");
+$routes->post("/logout", "AuthController::logoutUser");

bf4cfb07d2ac3449e3a2aac52289c9b46a669fbd
gabcytn <cayetanogabriel03@gmail.com>
2024-11-16 23:31:14 +0800

    Load a default profile picture on side nave

---
+++ app/Views/includes/side-nav.php
@@ -0,0 +1,9 @@
+<div class="side-nav">
+    <h1>Instaclone</h1>
+    <ul class="side-nav-list">
+        <li class="side-nav-item"><ion-icon name="home"></ion-icon><a href="<?= base_url("/") ?>" class="side-nav-link">Home</a></li>
+        <li class="side-nav-item"><ion-icon name="search-outline"></ion-icon><a href="#" class="side-nav-link">Search</a></li>
+        <li class="side-nav-item"><ion-icon name="add-circle-outline"></ion-icon><a href="#" class="side-nav-link">Create</a></li>
+        <li class="side-nav-item"><img src="<?= base_url("assets/default-profile.jpg") ?>" alt="Profile Picture" id="side-nav-profile"><a href="#" class="side-nav-link">Profile</a></li>
+    </ul>
+</div>

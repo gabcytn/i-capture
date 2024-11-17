70ff03885ad2a5eb8a1411ed37370b9ceae5dde0
gabcytn <cayetanogabriel03@gmail.com>
2024-11-17 16:39:32 +0800

    Temporary views for posts; TOOD

---
+++ app/Views/posts-views/your-posts.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Instaclone</title>
+    <?php include "app/Views/includes/bootstrap.php" ?>
+    <?php include "app/Views/includes/lobster-two.php" ?>
+    <?php include "app/Views/includes/ionicons.php"; ?>
+    <link rel="stylesheet" href="<?= base_url("assets/css/side-nav.css"); ?> " />
+
+    <style>
+        .container {
+            max-width: 650px !important;
+        }
+    </style>
+</head>
+<body>
+    <?php include "app/Views/includes/side-nav.php" ?>
+    <div class="container">
+        <div class="row">
+            <h1>TODO: Add Post Contents</h1>
+        </div>
+    </div>
+</body>
+</html>

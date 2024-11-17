74647cf3f4a437bdd7cfa7fd72b93de7ac89a85a
gabcytn <cayetanogabriel03@gmail.com>
2024-11-17 14:44:57 +0800

    Create a directory and file for user accounts views

---
+++ app/Views/user-views/your-profile.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@<?= session()->get("username") ?></title>
+    <?php include "app/Views/includes/bootstrap.php" ?>
+    <?php include "app/Views/includes/lobster-two.php" ?>
+    <?php include "app/Views/includes/ionicons.php"; ?>
+    <link rel="stylesheet" href="<?= base_url("assets/css/side-nav.css"); ?> " />
+
+    <style>
+        .container {
+            max-width: 650px !important;
+        }
+        .col-4 img {
+            width: 10rem;
+            border-radius: 100%;
+        }
+    </style>
+</head>
+<body>
+    <?php include "app/Views/includes/side-nav.php" ?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-4">
+                <img src="<?= base_url("assets/default-profile.jpg"); ?>" alt="Profile Picture"/>
+            </div>
+            <div class="col-8">
+                <div class="">
+                    <div class="d-flex gap-3">
+                        <p style="align-self: center; margin: 0; font-weight: 600;">@<?= esc(session()->get("username")) ?></p>
+                        <button class="btn btn-secondary">Edit Profile</button>
+                    </div>
+                    <div class="d-flex gap-5 mt-3">
+                        <p>0 posts</p>
+                        <p>100 followers</p>
+                        <p>66 following</p>
+                    </div>
+                </div>
+            </div>
+            <hr class="mt-4"/>
+        </div>
+    </div>
+</body>
+</html>

e41c09335f9f81e6be8ca0c8fb1bebf7bf4d104b
gabcytn <cayetanogabriel03@gmail.com>
2024-11-17 14:42:55 +0800

    Return a profile view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,9 @@ class Home extends BaseController
     {
         return view("home");
     }
+
+    public function profile(): string
+    {
+        return view("user-views/your-profile.php");
+    }
 }

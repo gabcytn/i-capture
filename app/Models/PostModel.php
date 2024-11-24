b8381135fd95595ebaee1b90ca613405a7edd047
gabcytn <cayetanogabriel03@gmail.com>
2024-11-24 23:41:31 +0800

    Crated a query on retrieving posts excluding the ones already given

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -34,9 +34,39 @@ class PostModel extends Model
         $this->db->query("UPDATE posts SET likes = likes + 1 WHERE id = ?", [$postId]);
     }
 
-    public function findAllWherePostOwnerNotEqualTo (string $username): array
+    public function findAllWherePostOwnerNotEqualTo (string $username, array $array = []): array
     {
-        $resultSet = $this->db->query("SELECT * FROM posts WHERE post_owner != ? ORDER BY RAND() LIMIT 10", [$username]);
+        $postIdPlaceholder = $this->createStringArrayFromIntArray($array);
+        $sql =
+            "SELECT users.id AS username,
+            users.profile_pic, posts.id AS post_id,
+            posts.likes, posts.photo_url
+            FROM users
+            INNER JOIN posts
+            ON users.id = posts.post_owner
+            WHERE users.id != ?
+            AND posts.id NOT IN ($postIdPlaceholder)
+            ORDER BY RAND()
+            LIMIT 10";
+
+        $resultSet = $this->db->query($sql, $username);
         return $resultSet->getResult();
+    }
+
+    private function createStringArrayFromIntArray ($nums): string
+    {
+        if (sizeof($nums) == 0) return "0";
+
+        $str = "";
+        for ($i = 0; $i < sizeof($nums); $i++) {
+            if ($i == sizeof($nums) - 1) {
+                $str .= $nums[$i];
+                return $str;
+            }
+
+            $str .= $nums[$i] . ", ";
+        }
+
+        return "0";
     }
 }

c5fccbc6ad0e9e038ccde4791248467a300ce491
gabcytn <cayetanogabriel03@gmail.com>
2024-11-27 22:41:56 +0800

    Change: separate the methods called for initial posts and additional posts
    Save the initial posts on the session key 'posts' and append to it when requesting for more posts. Get the value when querying the database to avoid duplicates of posts

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -11,32 +11,53 @@ class PostModel extends Model
     protected $allowedFields = ["post_owner", "likes", "photo_url", "photo_public_id"];
     protected bool $updateOnlyChanged = true;
 
-    public function findAllByPostOwnerUsername (string $username): array
+    public function findAllByPostOwnerUsername(string $username): array
     {
         $data = $this->db->query("SELECT * FROM posts WHERE post_owner = ?", [$username]);
 
         return $data->getResult();
     }
 
-    public function getPostCount (string $username): int
+    public function getPostCount(string $username): int
     {
         $resultSet = $this->db->query("SELECT * FROM posts WHERE post_owner = ?", [$username]);
         return $resultSet->getNumRows();
     }
 
-    public function decreaseLikes ($postId): void
+    public function decreaseLikes($postId): void
     {
         $this->db->query("UPDATE posts SET likes = likes - 1 WHERE id = ?", [$postId]);
     }
 
-    public function increaseLikes ($postId): void
+    public function increaseLikes($postId): void
     {
         $this->db->query("UPDATE posts SET likes = likes + 1 WHERE id = ?", [$postId]);
     }
 
-    public function findAllNotLikedBy(string $username, array $array = []): array
+    public function findAllNotLikedBy(string $username): array
     {
-        $postIdPlaceholder = $this->createStringArrayFromIntArray($array);
+        $sql =
+            "SELECT posts.id AS post_id, posts.post_owner, posts.likes, posts.photo_url, users.profile_pic
+            FROM posts 
+            INNER JOIN users 
+            ON posts.post_owner = users.id
+            LEFT JOIN likes
+            ON likes.post_id = posts.id AND likes.liker_id = ?
+            WHERE likes.post_id IS NULL
+            AND posts.post_owner != ?
+            ORDER BY RAND()
+            LIMIT 10";
+
+        $resultSet = $this->db->query($sql, [$username, $username]);
+        $posts = $resultSet->getResult();
+
+        $this->saveIdsToSession($posts);
+        return $posts;
+    }
+
+    public function findPostsNotIn(array $postIds = [], string $username)
+    {
+        $postIdPlaceholder = $this->createStringArrayFromIntArray($postIds);
         $sql =
             "SELECT posts.id AS post_id, posts.post_owner, posts.likes, posts.photo_url, users.profile_pic
             FROM posts 
@@ -51,10 +72,13 @@ class PostModel extends Model
             LIMIT 10";
 
         $resultSet = $this->db->query($sql, [$username, $username]);
-        return $resultSet->getResult();
+        $posts = $resultSet->getResult();
+
+        $this->appendIdsToSession($posts);
+        return $posts;
     }
 
-    private function createStringArrayFromIntArray (array $nums): string
+    private function createStringArrayFromIntArray(array $nums): string
     {
         if (sizeof($nums) == 0) return "0";
 
@@ -69,5 +93,25 @@ class PostModel extends Model
         }
 
         return "0";
+    }
+
+    private function saveIdsToSession(array $initialPosts): void
+    {
+        $ids = [];
+        foreach ($initialPosts as $initialPost) {
+            array_push($ids, $initialPost->post_id);
+        }
+
+        session()->set("posts", $ids);
+    }
+
+    private function appendIdsToSession(array $additionalPosts)
+    {
+        $ids = session()->get("posts");
+        foreach ($additionalPosts as $additionalPost) {
+            array_push($ids, $additionalPost->post_id);
+        }
+
+        session()->set("posts", $ids);
     }
 }

0088ce6d602297f5dafa3ed3b250f0b3f0c5e772
gabcytn <cayetanogabriel03@gmail.com>
2024-12-01 00:18:20 +0800

    Auto reformat by neovim

--- app/Models/FollowerModel.php
+++ app/Models/FollowerModel.php
@@ -10,30 +10,30 @@ class FollowerModel extends Model
     protected $allowedFields = ["following_id", "follower_id"];
     protected bool $updateOnlyChanged = true;
 
-    public function getFollowerCount (string $username): int
+    public function getFollowerCount(string $username): int
     {
         $resultSet = $this->db->query("SELECT * FROM followers WHERE following_id = ?", [$username]);
         return $resultSet->getNumRows();
     }
 
-    public function getFollowingCount (string $username): int
+    public function getFollowingCount(string $username): int
     {
         $resultSet = $this->db->query("SELECT * FROM followers WHERE follower_id = ?", [$username]);
         return $resultSet->getNumRows();
     }
 
-    public function isFollowing ($followingId, $followerId): bool
+    public function isFollowing($followingId, $followerId): bool
     {
         $resultSet = $this->db->query("SELECT * FROM followers WHERE follower_id = ? AND following_id = ?", [$followerId, $followingId]);
         return $resultSet->getNumRows() == 1;
     }
 
-    public function removeFollower ($followingId, $followerId): void
+    public function removeFollower($followingId, $followerId): void
     {
         $this->db->query("DELETE FROM followers WHERE follower_id = ? AND following_id = ?", [$followerId, $followingId]);
     }
 
-    public function getFollowersList ($username): array
+    public function getFollowersList($username): array
     {
         $resultSet = $this->db->query("
                     SELECT users.id, users.profile_pic
@@ -44,7 +44,7 @@ class FollowerModel extends Model
         return $resultSet->getResult();
     }
 
-    public function getFollowingList ($username): array
+    public function getFollowingList($username): array
     {
         $resultSet = $this->db->query("
                     SELECT users.id, users.profile_pic
@@ -56,3 +56,4 @@ class FollowerModel extends Model
         return $resultSet->getResult();
     }
 }
+

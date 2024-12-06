303d7ce8e26f4523212dd13ae1af614d233e1dc3
gabcytn <cayetanogabriel03@gmail.com>
2024-12-06 20:16:03 +0800

    Major refactor: migrate to java springboot backend

---
+++ backend/src/main/java/com/gabcytn/i_capture/ICaptureApplication.java
@@ -0,0 +1,13 @@
+package com.gabcytn.i_capture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICaptureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICaptureApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabcytn/i_capture/ICaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabcytn.i_capture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICaptureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d42d69f75196a654e8396f612eeb1d97c300773a
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 00:24:49 +0800

    Created reusable layout for auth pages register and login

---
+++ frontend/src/layout/AuthLayout.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react"
+
+type AuthLayoutProps = {
+  children: ReactNode;
+  subText: string;
+  subTextHref: string;
+  subTextAnchor: string;
+}
+
+function AuthLayout({ children, subText, subTextHref, subTextAnchor }: AuthLayoutProps) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <img src="/auth-img.png" alt="Auth image" />
+        </div>
+        <div className="col-6">
+          <form style={form}>
+            <h3 style={heading} className="mb-5 text-center">iCapture</h3>
+            {children}
+          </form>
+
+          <div style={subTextsStyle} className="d-flex justify-content-center align-items-center p-3 mt-3">
+            <p>{subText}<a href={subTextHref}>{subTextAnchor}</a> </p>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+const subTextsStyle = {
+  border: "1px solid #CCC"
+}
+
+const heading = {
+  fontFamily: "Lobster Two",
+}
+
+const form = {
+  height: "400px",
+  border: "1px solid #CCC",
+  marginTop: "2.5rem",
+  padding: "2rem 1.5rem"
+}
+
+
+export default AuthLayout

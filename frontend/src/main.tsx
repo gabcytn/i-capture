e99eee61a5045ac80cdc83959858bb3069a03519
gabcytn <cayetanogabriel03@gmail.com>
2024-12-10 20:48:04 +0800

    Used <BrowserRouter /> for routing through the React app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './pages/App.tsx'
+import NotFound from './pages/NotFound.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 )

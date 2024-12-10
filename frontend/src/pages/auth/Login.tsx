fb9755198461375f1f3d793b1295ed2f146e4387
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 01:09:29 +0800

    Passed custom onSubmit function handlers

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+import { useNavigate } from "react-router"
 import AuthLayout from "../../layout/AuthLayout"
 import Button from "../../components/Button"
 import AuthTextInput from "../../components/AuthTextInput"
 
 function Login() {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${SERVER_URL}/login`)
+      if (!res.ok)
+        throw new Error(`Error status code of: ${res.status}`);
+
+      navigate("/");
+
+    } catch (error: unknown) {
+      if (error instanceof Error) alert(error.message);
+      else console.error("Unknown error on login")
+    }
+  }
+
   return (
-    <AuthLayout subText="Don't have an account? " subTextAnchor="Sign up" subTextHref="/register">
-      <AuthTextInput type="text" placeholder="Username" className="mt-5" />
-      <AuthTextInput type="password" placeholder="Password" className="mt-3" />
+    <AuthLayout subText="Don't have an account? " subTextAnchor="Sign up" subTextHref="/register" onSubmit={handleSubmit}>
+      <AuthTextInput value={username} onChange={setUsername} type="text" placeholder="Username" className="mt-5" />
+      <AuthTextInput value={password} onChange={setPassword} type="password" placeholder="Password" className="mt-3" />
       <Button title="Login" type="submit" className="w-100 mt-5" />
       <p className="text-center fs-10 mt-2"><a className="text-decoration-none" href="#">Forgot password?</a></p>
     </AuthLayout>
--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { useNavigate } from "react-router"
 import AuthLayout from "../../layout/AuthLayout"
 import Button from "../../components/Button"
 import AuthTextInput from "../../components/AuthTextInput"
 
 function Register() {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [confirmPass, setConfirmPass] = useState<string>("")
+  const navigate = useNavigate();
+  const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPass) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    try {
+      const res = await fetch(`${SERVER_URL}/login`)
+      if (!res.ok)
+        throw new Error(`Error status code of: ${res.status}`);
+
+      navigate("/login");
+
+    } catch (error: unknown) {
+      if (error instanceof Error) alert(error.message);
+      else console.error("Unknown error on login")
+    }
+
+  }
+
   return (
-    <AuthLayout subText="Already have an account? " subTextAnchor="Login" subTextHref="/login">
-      <AuthTextInput type="text" placeholder="Username" className="mt-5" />
-      <AuthTextInput type="password" placeholder="Password" className="mt-3" />
+    <AuthLayout subText="Already have an account? " subTextAnchor="Login" subTextHref="/login" onSubmit={handleSubmit}>
+      <AuthTextInput value={username} onChange={setUsername} type="text" placeholder="Username" className="mt-5" />
+      <AuthTextInput value={password} onChange={setPassword} type="password" placeholder="Password" className="mt-3" />
+      <AuthTextInput value={confirmPass} onChange={setConfirmPass} type="password" placeholder="Confirm Password" className="mt-3" />
       <Button title="Sign up" type="submit" className="w-100 mt-5" />
     </AuthLayout>
   )

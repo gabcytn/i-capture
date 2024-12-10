f8a9de6e914ef978201e8b59e9148c500658ab8a
gabcytn <cayetanogabriel03@gmail.com>
2024-12-10 20:47:38 +0800

    Created a page not found for routes not defined

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+function NotFound() {
+  return (
+    <div className="d-flex justify-content-center align-items-center" style={styles}>
+      <h3 className="text-center">404 Page Not Found</h3>
+    </div>
+  )
+}
+
+const styles = {
+  minHeight: "100dvh",
+}
+
+export default NotFound;

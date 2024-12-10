0b55a300f4bf68bccfb9b0cb8d46c40363c1bb12
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 00:26:07 +0800

    Created basic auth pages

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,6 +1,15 @@
+import AuthLayout from "../../layout/AuthLayout"
+import Button from "../../components/Button"
+import AuthTextInput from "../../components/AuthTextInput"
+
 function Login() {
   return (
-    <div>Login</div>
+    <AuthLayout subText="Don't have an account? " subTextAnchor="Sign up" subTextHref="/register">
+      <AuthTextInput type="text" placeholder="Username" className="mt-5" />
+      <AuthTextInput type="password" placeholder="Password" className="mt-3" />
+      <Button title="Login" type="submit" className="w-100 mt-5" />
+      <p className="text-center fs-10 mt-2"><a className="text-decoration-none" href="#">Forgot password?</a></p>
+    </AuthLayout>
   )
 }
 
--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,6 +1,14 @@
+import AuthLayout from "../../layout/AuthLayout"
+import Button from "../../components/Button"
+import AuthTextInput from "../../components/AuthTextInput"
+
 function Register() {
   return (
-    <div>Register</div>
+    <AuthLayout subText="Already have an account? " subTextAnchor="Login" subTextHref="/login">
+      <AuthTextInput type="text" placeholder="Username" className="mt-5" />
+      <AuthTextInput type="password" placeholder="Password" className="mt-3" />
+      <Button title="Sign up" type="submit" className="w-100 mt-5" />
+    </AuthLayout>
   )
 }
 

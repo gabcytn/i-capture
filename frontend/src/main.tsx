9d8c5ab0bc13c6044de38f55089f46a9aeb7f99a
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 00:22:19 +0800

    Added routes for /register and /login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import App from './pages/App.tsx'
+import Login from './pages/auth/Login.tsx'
+import Register from './pages/auth/Register.tsx'
 import NotFound from './pages/NotFound.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   </BrowserRouter>

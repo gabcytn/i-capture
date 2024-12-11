653c25f5e88bf855b535f512dd8221242fa77c3a
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 22:34:01 +0800

    Extracted main file into its own component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 // import { StrictMode } from 'react'
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import App from './pages/App.tsx'
@@ -6,13 +7,24 @@ import Login from './pages/auth/Login.tsx'
 import Register from './pages/auth/Register.tsx'
 import NotFound from './pages/NotFound.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-)
+const Main = () => {
+  useEffect(() => {
+    const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+    localStorage.setItem("serverUrl", SERVER_URL);
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Main;
+
+createRoot(document.getElementById('root')!).render(<Main />)

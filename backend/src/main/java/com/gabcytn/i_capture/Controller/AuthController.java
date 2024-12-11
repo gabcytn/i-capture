9c491aacf2db83820999355e0604a4c2e4a2258a
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 22:36:00 +0800

    Renamed to AuthController

---
+++ backend/src/main/java/com/gabcytn/i_capture/Controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.gabcytn.i_capture.Controller;
+
+import com.gabcytn.i_capture.Model.LoginRequest;
+import com.gabcytn.i_capture.Model.User;
+import com.gabcytn.i_capture.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService,
+                          AuthenticationManager authenticationManager,
+                          SecurityContextRepository securityContextRepository
+    ) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser () {
+        return new User();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById (@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/session-id")
+    public String getSessionId (HttpServletRequest request){
+        return request.getSession().getId();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
+    {
+        return userService.handleAuthentication(loginRequest, request, response);
+    }
+}

39fb71aea062790f0b6172a58118a098fd8ff50c
gabcytn <cayetanogabriel03@gmail.com>
2024-12-11 22:57:14 +0800

    Inject a PasswordEncoder and encrypt the request's password before saving to database

--- backend/src/main/java/com/gabcytn/i_capture/Service/UserService.java
+++ backend/src/main/java/com/gabcytn/i_capture/Service/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,14 +22,17 @@ public class UserService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final SecurityContextRepository securityContextRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository,
                        AuthenticationManager authenticationManager,
-                       SecurityContextRepository securityContextRepository
+                       SecurityContextRepository securityContextRepository,
+                       PasswordEncoder passwordEncoder
     ) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.securityContextRepository = securityContextRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User getUserById (int id) {
@@ -66,5 +70,11 @@ public class UserService {
         {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public ResponseEntity<Void> registerUser (AuthRequest authRequest) {
+        // encrypt password with bcrypt (strength of 10)
+        authRequest.setPassword(passwordEncoder.encode(authRequest.getPassword()));
+        return userRepository.save(authRequest.getUsername(), authRequest.getPassword());
     }
 }

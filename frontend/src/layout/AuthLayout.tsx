3c9cc9b41264f7dd3b3c77d24cfaffea69fe1a35
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 18:06:48 +0800

    Explicitly state container's max-width

--- frontend/src/layout/AuthLayout.tsx
+++ frontend/src/layout/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type AuthLayoutProps = {
   children: ReactNode;
@@ -6,44 +6,57 @@ type AuthLayoutProps = {
   subTextHref: string;
   subTextAnchor: string;
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-}
+};
 
-function AuthLayout({ children, subText, subTextHref, subTextAnchor, onSubmit }: AuthLayoutProps) {
+function AuthLayout({
+  children,
+  subText,
+  subTextHref,
+  subTextAnchor,
+  onSubmit,
+}: AuthLayoutProps) {
   return (
-    <div className="container">
+    <div className="container mt-3" style={{ maxWidth: "750px " }}>
       <div className="row">
         <div className="col-6">
           <img src="/auth-img.png" alt="Auth image" />
         </div>
         <div className="col-6">
           <form style={form} onSubmit={onSubmit}>
-            <h3 style={heading} className="mb-5 text-center">iCapture</h3>
+            <h3 style={heading} className="mb-5 text-center">
+              iCapture
+            </h3>
             {children}
           </form>
 
-          <div style={subTextsStyle} className="d-flex justify-content-center align-items-center p-3 mt-3">
-            <p>{subText}<a href={subTextHref}>{subTextAnchor}</a> </p>
+          <div
+            style={subTextsStyle}
+            className="d-flex justify-content-center align-items-center p-3 mt-3"
+          >
+            <p>
+              {subText}
+              <a href={subTextHref}>{subTextAnchor}</a>{" "}
+            </p>
           </div>
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
 const subTextsStyle = {
-  border: "1px solid #CCC"
-}
+  border: "1px solid #CCC",
+};
 
 const heading = {
   fontFamily: "Lobster Two",
-}
+};
 
 const form = {
   height: "400px",
   border: "1px solid #CCC",
   marginTop: "2.5rem",
-  padding: "2rem 1.5rem"
-}
+  padding: "2rem 1.5rem",
+};
 
-
-export default AuthLayout
+export default AuthLayout;

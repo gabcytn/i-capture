86e2be3859a08eb0cea7e62a58931e2276c07511
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 12:38:41 +0800

    Added handling of user input in dialogs

--- frontend/src/components/SideNav/SideNav.tsx
+++ frontend/src/components/SideNav/SideNav.tsx
@@ -1,12 +1,16 @@
 import { CSSProperties, useState } from "react";
 import { useNavigate } from "react-router";
-import styles from "./SideNav.module.css"
+import styles from "./SideNav.module.css";
 import IonIcon from "@reacticons/ionicons";
-import DialogBox from "../FormDialogBox";
+import DialogBox from "../DialogBox";
+import Button from "../Button";
+import FormInput from "../FormInput";
 
 function SideNav() {
   const [isCreateDialogOpen, setIsCreateDialogOpen] = useState<boolean>(false);
-  const [isSearchDialogOpen, setIsSearchDialogOpen] = useState<boolean>(false)
+  const [isSearchDialogOpen, setIsSearchDialogOpen] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [file, setFile] = useState<File | null>(null);
 
   const username = sessionStorage.getItem("username")!;
   const navigate = useNavigate();
@@ -15,11 +19,11 @@ function SideNav() {
 
   const openSearchDialog = () => {
     setIsSearchDialogOpen(true);
-  }
+  };
 
   const closeSearchDialog = () => {
     setIsSearchDialogOpen(false);
-  }
+  };
 
   const closeCreateDialog = () => {
     setIsCreateDialogOpen(false);
@@ -40,11 +44,10 @@ function SideNav() {
         sessionStorage.clear();
         navigate("/login");
       }
-
     } catch (e: unknown) {
       if (e instanceof Error) alert(e.message);
     }
-  }
+  };
   return (
     <>
       <div className={styles.sideNav}>
@@ -52,45 +55,94 @@ function SideNav() {
         <ul className={styles.sideNavList}>
           <li className={styles.sideNavItem}>
             <IonIcon style={iconStyle} name="home" />
-            <a className={styles.sideNavLink} href="#">Home</a>
+            <a className={styles.sideNavLink} href="/">
+              Home
+            </a>
           </li>
           <li className={styles.sideNavItem} onClick={openSearchDialog}>
             <IonIcon style={iconStyle} name="search" />
-            <a className={styles.sideNavLink} href="#">Search</a>
+            <a className={styles.sideNavLink} href="#">
+              Search
+            </a>
           </li>
           <li className={styles.sideNavItem} onClick={openCreateDialog}>
             <IonIcon style={iconStyle} name="add-circle" />
-            <a className={styles.sideNavLink} href="#">Create</a>
+            <a className={styles.sideNavLink} href="#">
+              Create
+            </a>
           </li>
           <li className={styles.sideNavItem}>
-            <img src={proflePic} alt="profile pic" className={styles.sideNavProfile} />
-            <a className={styles.sideNavLink} href={username}>Profile</a>
+            <img
+              src={proflePic}
+              alt="profile pic"
+              className={styles.sideNavProfile}
+            />
+            <a className={styles.sideNavLink} href={username}>
+              Profile
+            </a>
           </li>
         </ul>
         <div className={styles.logoutForm}>
-          <button onClick={handleLogout} className="btn btn-danger">Logout</button>
+          <Button
+            title="Logout"
+            className="btn btn-danger"
+            handleClick={handleLogout}
+            type="button"
+          />
         </div>
-      </div >
-      <DialogBox
-        isOpen={isSearchDialogOpen}
-        onClose={closeSearchDialog}
-        title="Search User"
-        inputType="text"
-        placeholder="Search"
-      />
-      <DialogBox
-        isOpen={isCreateDialogOpen}
-        onClose={closeCreateDialog}
-        title="Create Post"
-        inputType="file"
-      />
+      </div>
+      <DialogBox isOpen={isSearchDialogOpen} title="Search User">
+        <FormInput
+          type="text"
+          placeholder="Search"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setSearchValue(e.target.value);
+          }}
+          value={searchValue}
+        />
+        <div className="d-flex mt-3">
+          <Button
+            title="Submit"
+            type="button"
+            className="btn btn-primary me-2"
+          />
+          <Button
+            title="Close"
+            className="btn btn-danger"
+            type="button"
+            handleClick={closeSearchDialog}
+          />
+        </div>
+      </DialogBox>
+      <DialogBox isOpen={isCreateDialogOpen} title="Create">
+        <FormInput
+          type="file"
+          onChange={(e) => {
+            if (e.target.files && e.target.files.length > 0)
+              setFile(e.target.files[0]);
+          }}
+        />
+        <div className="d-flex mt-3">
+          <Button
+            title="Submit"
+            type="button"
+            className="btn btn-primary me-2"
+          />
+          <Button
+            title="Close"
+            className="btn btn-danger"
+            type="button"
+            handleClick={closeCreateDialog}
+          />
+        </div>
+      </DialogBox>
     </>
-  )
+  );
 }
 
 const iconStyle: CSSProperties = {
   display: "flex",
   fontSize: "1.5rem",
-}
+};
 
-export default SideNav
+export default SideNav;

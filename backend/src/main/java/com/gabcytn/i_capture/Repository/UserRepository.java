be67b811751aa6745b8c2ab18fe332c6ce855e08
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 20:56:38 +0800

    Handled duplicate username errors

--- backend/src/main/java/com/gabcytn/i_capture/Repository/UserRepository.java
+++ backend/src/main/java/com/gabcytn/i_capture/Repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.gabcytn.i_capture.Repository;
 
 import com.gabcytn.i_capture.Model.User;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class UserRepository {
@@ -31,8 +30,12 @@ public class UserRepository {
         String sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
         try {
             jdbcTemplate.update(sqlQuery, username, password);
+        } catch (DuplicateKeyException e) {
+            System.err.println("Duplicate key exception");
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            System.err.println("Unknown error in registration");
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }

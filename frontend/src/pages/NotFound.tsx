bc135f26bae588ff8cfe54f2c09db98bc0b659a6
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 12:35:50 +0800

    config prettier

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,13 +1,16 @@
 function NotFound() {
   return (
-    <div className="d-flex justify-content-center align-items-center" style={styles}>
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={styles}
+    >
       <h3 className="text-center">404 Page Not Found</h3>
     </div>
-  )
+  );
 }
 
 const styles = {
   minHeight: "100dvh",
-}
+};
 
 export default NotFound;

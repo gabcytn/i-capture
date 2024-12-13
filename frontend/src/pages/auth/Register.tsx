0d430dfc4e1f11e1f4adf8979315eca1d0bda977
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 12:36:59 +0800

    Handled 409 conflict status

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { useNavigate } from "react-router"
-import AuthLayout from "../../layout/AuthLayout"
-import Button from "../../components/Button"
-import AuthTextInput from "../../components/AuthTextInput"
+import { useNavigate } from "react-router";
+import AuthLayout from "../../layout/AuthLayout";
+import Button from "../../components/Button";
+import FormInput from "../../components/FormInput";
 
 function Register() {
-  const [username, setUsername] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
-  const [confirmPass, setConfirmPass] = useState<string>("")
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPass, setConfirmPass] = useState<string>("");
   const navigate = useNavigate();
   const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
@@ -28,27 +28,59 @@ function Register() {
           username: username,
           password: password,
         }),
-      })
-      if (!res.ok)
-        throw new Error(`Error status code of: ${res.status}`);
+      });
+      if (res.status === 409) throw new Error("Username already exists");
+      if (res.status !== 201)
+        throw new Error(`Error status code of ${res.status}`);
 
       navigate("/login");
-
     } catch (error: unknown) {
       if (error instanceof Error) alert(error.message);
-      else console.error("Unknown error on login")
+      else console.error("Unknown error on login");
     }
-
-  }
+  };
 
   return (
-    <AuthLayout subText="Already have an account? " subTextAnchor="Login" subTextHref="/login" onSubmit={handleSubmit}>
-      <AuthTextInput value={username} onChange={setUsername} type="text" placeholder="Username" className="mt-5" />
-      <AuthTextInput value={password} onChange={setPassword} type="password" placeholder="Password" className="mt-3" />
-      <AuthTextInput value={confirmPass} onChange={setConfirmPass} type="password" placeholder="Confirm Password" className="mt-3" />
-      <Button title="Sign up" type="submit" className="w-100 mt-5" />
+    <AuthLayout
+      subText="Already have an account? "
+      subTextAnchor="Login"
+      subTextHref="/login"
+      onSubmit={handleSubmit}
+    >
+      <FormInput
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        type="text"
+        placeholder="Username"
+        className="mt-5"
+      />
+      <FormInput
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        type="password"
+        placeholder="Password"
+        className="mt-3"
+      />
+      <FormInput
+        value={confirmPass}
+        onChange={(e) => {
+          setConfirmPass(e.target.value);
+        }}
+        type="password"
+        placeholder="Confirm Password"
+        className="mt-3"
+      />
+      <Button
+        title="Sign up"
+        type="submit"
+        className="w-100 mt-5 btn btn-primary"
+      />
     </AuthLayout>
-  )
+  );
 }
 
-export default Register
+export default Register;

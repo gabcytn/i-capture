7bc7473b2e5aa837895521ca25185ea56a748e21
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 12:39:53 +0800

    Prettier automatic styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { createRoot } from 'react-dom/client'
-import App from './pages/App.tsx'
-import Login from './pages/auth/Login.tsx'
-import Register from './pages/auth/Register.tsx'
-import Profile from './pages/Profile.tsx'
-import NotFound from './pages/NotFound.tsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import { createRoot } from "react-dom/client";
+import App from "./pages/App.tsx";
+import Login from "./pages/auth/Login.tsx";
+import Register from "./pages/auth/Register.tsx";
+import Profile from "./pages/Profile.tsx";
+import NotFound from "./pages/NotFound.tsx";
 import PrivateRoute from "./route/PrivateRoute.tsx";
 import PublicRoute from "./route/PublicRoute.tsx";
 
@@ -16,17 +16,17 @@ const Main = () => {
           <Route path="/" element={<App />} />
           <Route path="/home" element={<App />} />
           <Route path="/:segment" element={<Profile />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
         <Route element={<PublicRoute />}>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Main;
 
-createRoot(document.getElementById('root')!).render(<Main />)
+createRoot(document.getElementById("root")!).render(<Main />);
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -3,7 +3,6 @@ import SideNav from "../components/SideNav/SideNav";
 function App() {
   const url = import.meta.env.VITE_SERVER_URL;
 
-
   const sendRequest = async () => {
     try {
       const res = await fetch(`${url}/session-id`, {
@@ -13,20 +12,21 @@ function App() {
       const data = await res.text();
       alert(`${res.status}: ${data}`);
     } catch (e) {
-      if (e instanceof Error)
-        alert(`Error in test request: ${e.message}`);
+      if (e instanceof Error) alert(`Error in test request: ${e.message}`);
     }
-  }
+  };
 
   return (
     <>
       <SideNav />
       <div className="container">
         <p>App</p>
-        <button onClick={sendRequest} className="btn btn-warning">Test request</button>
+        <button onClick={sendRequest} className="btn btn-warning">
+          Test request
+        </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

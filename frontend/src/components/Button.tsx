9ec9ba4718437ee5ba25b8ae226e6ced6b7b106d
gabcytn <cayetanogabriel03@gmail.com>
2024-12-13 12:34:42 +0800

    handleClick is optional

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,11 +2,14 @@ type ButtonProps = {
   title: string;
   type: "button" | "reset" | "submit";
   className: string;
-}
-function Button({ title, type, className }: ButtonProps) {
+  handleClick?: () => void;
+};
+function Button({ title, type, className, handleClick }: ButtonProps) {
   return (
-    <button className={className + " btn btn-primary"} type={type}>{title}</button>
-  )
+    <button className={className} type={type} onClick={handleClick}>
+      {title}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
